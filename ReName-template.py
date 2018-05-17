ca15a06154ff19832329103cbe54c82e27431b72
DESKTOP-TTUSLQH\Mogami <atsuyatsuchikawa@gmail.com>
2018-05-17 16:00:07 +0900

    aha

--- ReName-template.py
+++ ReName-template.py
@@ -1,8 +1,11 @@
 import glob, os
-files = [os.path.basename(r) for r in glob.glob('*')]
+files = [os.path.basename(r) for r in glob.glob('*')] 
+#スクリプトがあるフォルダ内のファイルおよびディレクトリーが取得される
+#置換:
+#検索:i.find("hoge") なかったら-1を返す
 i=0
 while i < len(files) :
-    if -1 != files[i].find("AMD"):
+    if -1 != files[i].find(".png"):
         print("find"+files[i])
     print(i)
     print(files[i])
